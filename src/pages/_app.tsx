3b8de8797ffb05cbf61a32a1b11df15b567679fe
Ricnaga <47759922+Ricnaga@users.noreply.github.com>
2021-04-08 00:11:14 -0300

    add main page | with responsiveness

---
+++ src/pages/_app.tsx
@@ -0,0 +1,14 @@
+import { AppProps } from "next/app"
+import { ChakraProvider } from "@chakra-ui/react"
+import { theme } from "../../styles/theme"
+import '../../styles/carousel.css'
+
+function MyApp({ Component, pageProps }: AppProps) {
+  return (
+    <ChakraProvider theme={theme}>
+      <Component {...pageProps} />
+    </ChakraProvider>
+  )
+}
+
+export default MyApp
---
+++ src/pages/index.tsx
@@ -0,0 +1,36 @@
+import { Box, Divider, Text } from '@chakra-ui/react';
+import { Banner } from '../components/Banner';
+import { Carousel } from '../components/Carousel';
+import { Header } from '../components/Header';
+import { Items } from '../components/Items';
+
+export default function Home() {
+  return (
+    <Box w="100vw">
+      <Header />
+      <Banner />
+      <Items />
+
+      <Divider
+        borderColor="dark.Headings and Text"
+        w={["3rem" ,"90px"]}
+        border={["1px", "2px"]}
+        mx="auto"
+      />
+
+      <Text
+        textAlign="center"
+        fontSize={["1.2rem","2.2rem"]}
+        color="dark.Headings and Text"
+        fontWeight="500"
+        my="3rem">
+        Vamos nessa?<br />
+        Então escolha seu continente
+        </Text>
+
+      <Carousel />
+
+    </Box>
+
+  )
+}
